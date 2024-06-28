4a5429dcec0595155d62cf60265905cc4cf7210b
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-28 09:51:40 +0100

    creation du projet angulare

---
+++ src/app/series-list/series-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-series-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './series-list.component.html',
+  styleUrl: './series-list.component.css'
+})
+export class SeriesListComponent {
+
+}
