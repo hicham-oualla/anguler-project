b8a13b3f0eb1031de6b54e3c8b7f7c88eac57f8c
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-28 10:56:07 +0100

    series ervice

--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Serie } from './serie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
+  private apiUrl = 'http://localhost:8084/api/series'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.apiUrl).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    // Handle client-side or network errors
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // Handle backend errors
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something went wrong; please try again later.');
+  }
 }
+
+
+
+
