b4ae3e8cf8304ac3cbd3c1f38adcad38bd1ad8ca
hicham-oualla <hich2022am.oualla@gmail.com>
2024-06-28 10:05:44 +0100

    creation series model

---
+++ src/app/serie.ts
@@ -0,0 +1,18 @@
+export interface Serie {
+
+    
+    id_serie:number;
+    titre:string;
+   description :string;
+   date_debut:Date;
+   datefin :Date;
+   saison:string  ;
+   genre:string;
+   createur:string;
+   acteur:string;
+   note :string;
+
+
+
+
+}
